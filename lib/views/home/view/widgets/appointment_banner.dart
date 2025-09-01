b967de33fe9b1a27f09985b45043e19e6ca87490
Vincent140206 <vincentiusdylan01@gmail.com>
2025-09-01 09:53:15 +0700

    adding center layout

--- lib/views/home/view/widgets/appointment_banner.dart
+++ lib/views/home/view/widgets/appointment_banner.dart
@@ -5,14 +5,8 @@ class _AppointmentBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery
-        .of(context)
-        .size
-        .width;
-    final screenHeight = MediaQuery
-        .of(context)
-        .size
-        .height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Container(
       width: double.infinity,
       padding: EdgeInsets.symmetric(
@@ -23,30 +17,28 @@ class _AppointmentBanner extends StatelessWidget {
         color: const Color(0xFF007FFF),
         borderRadius: BorderRadius.circular(screenWidth * 0.04),
       ),
-      child: Center(
-        child: Row(
-          children: [
-            Image.asset(
-              'assets/images/Icon_noAppointment.png',
-              width: screenWidth * 0.1,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/Icon_noAppointment.png',
+            width: screenWidth * 0.1,
+          ),
+          SizedBox(width: screenWidth * 0.04),
+          Text(
+            'Belum ada Appointment!',
+            style: Theme
+                .of(context)
+                .textTheme
+                .bodyMedium
+                ?.copyWith(
+              color: Colors.white,
+              fontSize: screenWidth * 0.04,
+              fontWeight: FontWeight.w500,
             ),
-            SizedBox(width: screenWidth * 0.04),
-            Expanded(
-              child: Text(
-                'Belum ada Appointment!',
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .bodyMedium
-                    ?.copyWith(
-                  color: Colors.white,
-                  fontSize: screenWidth * 0.04,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
