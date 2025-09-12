0c2bd8564dc5c611eff06f3fbe31151f0952c7cf
Vincent140206 <vincentiusdylan01@gmail.com>
2025-09-12 07:14:38 +0700

    logic for saving clinic data

---
+++ lib/core/utils/storage_common.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:toothy/data/models/clinic.dart';
+
+class StorageCommon {
+  static Future<void> saveClinics(List<Clinic> clinics) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(
+      'clinics',
+      jsonEncode(clinics.map((e) => e.toJson()).toList()),
+    );
+  }
+
+  static Future<List<Clinic>> getClinics() async {
+    final prefs = await SharedPreferences.getInstance();
+    final clinicJson = prefs.getString('clinics');
+    if (clinicJson == null) return [];
+    final decoded = jsonDecode(clinicJson) as List;
+    return decoded.map((e) => Clinic.fromJson(e)).toList();
+  }
+
+  static Future<void> clearClinic() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('clinic');
+  }
+}
